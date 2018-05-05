019dd484ae65ba4be87f74e37daebc2d20a94aca
Krayeu Uladzislau <kraevroot@gmail.com>
2018-05-05 02:55:45 -0700

    fix images_joiner for one image

--- images_joiner.py
+++ images_joiner.py
@@ -13,20 +13,25 @@ class ImagesJoiner():
         
     def join_images(self):
         main_image = self.screens_pathes[0]
+
         main_image_path = True
-        for screen in self.screens_pathes[1:]:
-            if main_image_path:
-                img1 = cv2.imread(main_image, 0)
-                self.height, self.width = img1.shape
-                
-                img2 = cv2.imread(screen, 0)
-                new_img = np.concatenate((img1, img2), axis=0)
-                main_image = new_img
-                main_image_path = False
-            else:
-                img1 = main_image
-                img2 = cv2.imread(screen, 0)
-                new_img = np.concatenate((img1, img2), axis=0)
-                main_image = new_img
-        print("JOIN DONE")
+        if self.screens_count > 1:
+            for screen in self.screens_pathes[1:]:
+                if main_image_path:
+                    img1 = cv2.imread(main_image, 0)
+                    self.height, self.width = img1.shape
+                    
+                    img2 = cv2.imread(screen, 0)
+                    new_img = np.concatenate((img1, img2), axis=0)
+                    main_image = new_img
+                    main_image_path = False
+                else:
+                    img1 = main_image
+                    img2 = cv2.imread(screen, 0)
+                    new_img = np.concatenate((img1, img2), axis=0)
+                    main_image = new_img
+            return main_image
+        
+        main_image = cv2.imread(main_image, 0)
+        self.height, self.width = main_image.shape
         return main_image
